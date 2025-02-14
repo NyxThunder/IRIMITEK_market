230d7bd7d6c46d1baca51678094aad1b736cc260
prodevkitty <hk.talent55@gmail.com>
2025-02-13 21:37:04 -0600

    model modified

--- backend/model/apiModel.js
+++ backend/model/apiModel.js
@@ -1,91 +1,19 @@
 const mongoose  = require("mongoose");
-const productSchema = mongoose.Schema({
+const apiSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: [true, "Please Enter prodcut name"],
+    required: [true, "Please Enter api name"],
     trim: true,
   },
-  description: {
+  clientId: {
     type: String,
-    required: [true, "Please Enter prodcut description"],
+    required: [true, "Please Enter client ID"],
   },
-  price: {
-    type: Number,
-    required: [true, "Please Enter product Price"],
-    maxLength: [8, "Price cannot exceed 9 characters"],
-  },
-  info: {
+  clientSecret: {
     type: String,
-    required: [true, "Please Enter product info"],
+    required: [true, "Please Enter client Secret"],
   },
-
-  ratings: {
-    type: Number,
-    default: 0,
-  },
-  images: [
-    {
-      product_id: {
-        type: String,
-        required: true,
-      },
-      url: {
-        type: String,
-        required: true,
-      },
-    },
-  ],
-  category: {
-    type: String,
-    required: [true, "Please eneter Product Category"],
-  },
-  Stock: {
-    type: Number,
-    required: [true, "please Enter product stock"],
-    maxLength: [4, "Stock cannot exceed 4 characters"],
-    default: 1,
-  },
-  numOfReviews: {
-    type: Number,
-    default: 0,
-  },
-  reviews: [
-    {
-      userId: {
-        type: mongoose.Schema.ObjectId,
-        ref: "userModel",
-        required: true,
-      },
-      name: {
-        type: String,
-        required: true,
-      },
-      ratings: {
-        type: Number,
-        required: true,
-      },
-      title: {
-        type: String,
-        required: true,
-      },
-      comment: {
-        type: String,
-        required: true,
-      },
-      recommend: {
-        type: Boolean,
-        default: true,
-      },
-      createdAt: {
-        type: Date,
-        default: Date.now,
-      },
-      avatar: {
-        type: String,
-        required: true,
-      },
-    },
-  ],
+  
   // when two admins are there. tab ye pta chalgea kiss admin ne product add kiya hai
   user: {
     type: mongoose.Schema.ObjectId, //  this is for admin who will add the prduct to the db
@@ -98,5 +26,6 @@ const productSchema = mongoose.Schema({
   },
 });
 
-const ProductModel  = mongoose.model("ProductModel" , productSchema);
-module.exports =ProductModel
+const apiModel = mongoose.models.apiModel || mongoose.model("apiModel", apiSchema);
+
+module.exports =apiModel
